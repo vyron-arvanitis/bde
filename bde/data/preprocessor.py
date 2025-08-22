a7a26bb427cc87bcc413715ad6de2c6daf830a65
Vyron Arvanitis <vyron.arvanitis@cern.ch>
2025-08-22 18:11:53 +0300

    preprocessor.py sort of ready

--- bde/data/preprocessor.py
+++ bde/data/preprocessor.py
@@ -6,13 +6,27 @@ from dataloader import DataLoader
 class DataPreProcessor:
     def __init__(self, data: DataLoader):
         self.data = data
-        pass
 
     def norm_data(self, data: DataLoader) -> DataLoader:
+        #TODO: code
+
         pass
 
     def aug_data(self, data: DataLoader) -> DataLoader:
+        #TODO: code
+
         pass
 
     def add_noise_data(self, data: DataLoader) -> DataLoader:
+        #TODO: code
+
+        pass
+
+    def split(self):
+        """This method splits the data train, validate, test or train test
+        Returns
+        -------
+
+        """
+        #TODO: code
         pass
