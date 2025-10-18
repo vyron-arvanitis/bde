0498f6bb2fa8705debf4054efcfeb3447c0a5e07
Vyronas Arvanitis <vyronas.arvanitis@gmail.com>
2025-10-18 12:06:41 +0300

    cleaning up!

--- bde/sampler/types.py
+++ bde/sampler/types.py
@@ -5,6 +5,8 @@ directory layout when persisting samples (kept for potential future use), and
 `PRNGKey` annotates JAX pseudo-random keys.
 """
 
+import enum
+import sys
 import typing
 from pathlib import Path
 
@@ -16,3 +18,52 @@ ParamTree: typing.TypeAlias = dict[str, typing.Union[jax.Array, "ParamTree"]]
 FileTree: typing.TypeAlias = dict[str, typing.Union[Path, "FileTree"]]
 # A `PRNGKey` is a uint32 array of shape (2,) produced by `jax.random.PRNGKey`.
 PRNGKey = jax.Array
+
+
+class CustomEnumMeta(enum.EnumMeta):
+    """Custom Enum Meta Class for Better Error Handling."""
+
+    def __call__(cls, value, **kwargs):
+        """Extend the __call__ method to raise a ValueError."""
+        if value not in cls._value2member_map_:
+            raise ValueError(
+                f"{cls.__name__} must be one of {[*cls._value2member_map_.keys()]}"
+            )
+        return super().__call__(value, **kwargs)
+
+
+class BaseEnum(enum.Enum, metaclass=CustomEnumMeta):
+    """BaseEnum Class for implementing custom Enum classes."""
+
+
+if sys.version_info >= (3, 11):
+
+    class BaseStrEnum(enum.StrEnum, metaclass=CustomEnumMeta):
+        """BaseEnum Class for implementing custom Enum classes."""
+
+        def __str__(self):
+            """Return the string representation of the Enum."""
+            return self.value
+
+    class BaseIntEnum(enum.IntEnum, metaclass=CustomEnumMeta):
+        """BaseEnum Class for implementing custom Enum classes."""
+
+        def __str__(self):
+            """Return the string representation of the Enum."""
+            return self.value.__str__()
+
+else:
+
+    class BaseStrEnum(str, BaseEnum):
+        """BaseEnum Class for implementing custom Enum classes."""
+
+        def __str__(self):
+            """Return the string representation of the Enum."""
+            return self.value
+
+    class BaseIntEnum(int, BaseEnum):
+        """BaseEnum Class for implementing custom Enum classes."""
+
+        def __str__(self):
+            """Return the string representation of the Enum."""
+            return self.value.__str__()
